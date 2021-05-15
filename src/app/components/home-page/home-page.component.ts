ebb15994015a6182517e1a88f57691c0c5ef6c69
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-05-14 21:59:56 -0400

    initial implementation of dropdown animation; need to fix timing of dropdown

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  isMenuOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleHamburger(dropdownMenu: HTMLElement) {
+    dropdownMenu.classList.add('addTransitionSpeed');
+
+    if (this.isMenuOpen === false) {
+      dropdownMenu.classList.add('displayDropdown');
+      dropdownMenu.classList.remove('hideDropdown');
+      this.isMenuOpen = true;
+    } else if (this.isMenuOpen === true) {
+      dropdownMenu.classList.add('hideDropdown');
+      dropdownMenu.classList.remove('displayDropdown');
+      this.isMenuOpen = false;
+    }
+    setTimeout(function(){ 
+      dropdownMenu.classList.remove('addTransitionSpeed');
+    }, 500);
+  }
 }
