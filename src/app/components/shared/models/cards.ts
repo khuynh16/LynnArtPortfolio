18b226c3db1a687b394c849d4d28514f670be9f4
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-05-30 16:04:13 -0400

    created isolated categories for pictures in assets

--- src/app/components/shared/models/cards.ts
+++ src/app/components/shared/models/cards.ts
@@ -2,65 +2,65 @@ import { Card } from '../interfaces/card';
 
 export let CARDS: Card[] = [
     {
-      src: "../../../assets/images/still_life.jpg",
+      src: "../../../assets/images/traditional/painting/still_life.jpg",
       alt: "piece of multiple layers",
       category: "traditional",
       subcategory: "painting"
     },
     {
-      src: "../../../assets/images/artist_collaboration.jpg",
+      src: "../../../assets/images/traditional/painting/artist_collaboration.jpg",
       alt: "piece of multiple layers",
       category: "traditional",
       subcategory: "painting"
     },
     {
-      src: "../../../assets/images/figure.jpg",
+      src: "../../../assets/images/traditional/painting/figure.jpg",
       alt: "piece of multiple layers",
       category: "traditional",
       subcategory: "painting"
     },
     {
-      src: "../../../assets/images/landscape.jpg",
+      src: "../../../assets/images/traditional/painting/landscape.jpg",
       alt: "piece of multiple layers",
       category: "traditional",
       subcategory: "painting"
     },
     {
-      src: "../../../assets/images/place_mapping.jpg",
+      src: "../../../assets/images/traditional/paper_craft/place_mapping.jpg",
       alt: "piece of multiple layers",
       category: "traditional",
       subcategory: "paper craft"
     },
     {
-      src: "../../../assets/images/place_mapping_closeup.jpg",
+      src: "../../../assets/images/traditional/paper_craft/place_mapping_closeup.jpg",
       alt: "piece of multiple layers",
       category: "traditional",
       subcategory: "paper craft"
     },
     {
       src: "https://www.youtube.com/watch?v=4Nlcbl9-1Z8",
-      thumbnail: "../../../assets/images/visualizing_sound.jpg", 
+      thumbnail: "../../../assets/images/digital/video/visualizing_sound.jpg", 
       alt: "youtube video",
       category: "digital",
       subcategory: "video"
     },
     {
       src: "https://www.youtube.com/watch?v=y7NoS3GUtZc",
-      thumbnail: "../../../assets/images/flashback.jpg", 
+      thumbnail: "../../../assets/images/digital/video/flashback.jpg", 
       alt: "youtube video",
       category: "digital",
       subcategory: "video"
     },
     {
       src: "https://www.youtube.com/watch?v=2KbkUySv6RM",
-      thumbnail: "../../../assets/images/friendship.jpg", 
+      thumbnail: "../../../assets/images/digital/video/friendship.jpg", 
       alt: "youtube video",
       category: "digital",
       subcategory: "video"
     },
     {
       src: "https://www.youtube.com/watch?v=B2_WuPjRtJQ",
-      thumbnail: "../../../assets/images/behind_the_scenes.jpg", 
+      thumbnail: "../../../assets/images/digital/video/behind_the_scenes.jpg", 
       alt: "youtube video",
       category: "digital",
       subcategory: "video"
