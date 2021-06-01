029c36e9ac1e1e8a600caaacbb4d406b8c5dd344
Kelvin Huynh <kelvinahuynh@gmail.com>
2021-06-01 01:48:54 -0400

    implemented initial loaded routing to not include home-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lynn-art-portfolio';
+  constructor(private router: Router) {
+    this.router.events.subscribe(routerEvent => {
+        if (routerEvent instanceof NavigationStart) {
+            if (routerEvent.url == "/") {
+                this.router.navigate(["home-page"], {skipLocationChange: true})
+            }
+        }
+    });
+}
 }
